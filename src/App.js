d9223a1ade1ca0a4b583e060c10dfc2c8d4c1074
Percy Tejada <ppj.code@gmail.com>
2022-01-29 12:25:22 -0500

    Change frontend dir

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import FormTask from "./components/FormTask";
 import ListTask from "./components/ListTask";
 import Navbar from "./components/Navbar";
+import { Container } from "@mui/material";
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path={"/"} element={<ListTask />} />
-        <Route path={"/tasks/new"} element={<FormTask />} />
-      </Routes>
+      <Navbar />
+      <Container>
+        <Routes>
+          <Route path={"/"} element={<ListTask />} />
+          <Route path={"/tasks/new"} element={<FormTask />} />
+          <Route path={"/tasks/:id/edit"} element={<FormTask />} />
+        </Routes>
+      </Container>
     </BrowserRouter>
   );
 }
